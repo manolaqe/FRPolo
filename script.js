c88c71c89dc9870e809084c5a1b73a9b9154bdb1
Maria Tanasă <mariatanasa2000@gmail.com>
2021-05-07 19:48:39 +0300

    Adaugare stiri + modificare bazin

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 200) /*height in pixels when the navbar becomes non opaque*/ {
+    if ($(this).scrollTop() > 50) /*height in pixels when the navbar becomes non opaque*/ {
         $('.navbar').addClass('color-bg');
         $('.navbar').removeClass('no-bg');
     } else {
