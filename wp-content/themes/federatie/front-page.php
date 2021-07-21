114472122c990004aab54e9f1d2230cbcd07729d
manolaqe <manolachemihaiandrei@protonmail.ch>
2021-07-21 16:14:59 +0300

    conversie 2.5
    navbar dinamic, posts pe front page dinamice

--- wp-content/themes/federatie/front-page.php
+++ wp-content/themes/federatie/front-page.php
@@ -1,14 +1,18 @@
 <?php
     get_header();
 ?>
+<div>
     <!-- Hero image+text -->
     <div id="intro" class="view">
         <div class="mask rgba-black-strong ">
-            <div class="container-fluid d-flex align-items-center justify-content-center h-50">
-                <div class="row d-flex justify-content-center text-center ">
-                    <div class="col-md-12 ">
+            <div class="container-fluid d-flex align-items-center justify-content-center h-100">
+                <div class=" justify-content-center text-center ">
+                    <div class="col-md-12 mt-5 ">
+                        
                         <h2 class="display-5 font-weight-bold white-text pt-5 mb-2 ">FEDERAȚIA ROMÂNĂ DE POLO</h2>
                         <hr>
+
+                        <iframe width="896" height="504" src="https://www.youtube.com/embed/o-3snNxVz2A?start=1" title="YouTube video player" frameborder="0" allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture" autoplay; allowfullscreen></iframe>
 
                         <h4 class="white-text my-4 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti consequuntur.
                         </h4>
@@ -20,93 +24,49 @@
     </div>
 
     <!-- Stiri -->
-    <div class="container card-body mt-5 mb-5">
-        <br>
-        <br>
+    <div>
+    
+        <?php
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 3
+        );
+        $_posts = new WP_Query($args);
+        ?>
 
-        <div class="row mt-5 ">
-            <div class="col-sm-3 grid-margin">
-                <div class="position-relative">
-                    <div class="rotate-img">
-                        <img src="<?php echo get_template_directory_uri(); ?>./img/download.jfif" alt="thumb" class="img-fluid">
-                    </div>
+        <?php if ($_posts->have_posts()): ?>
 
-                </div>
-            </div>
-            <div class="col-sm-9 grid-margin text-left">
-                <h4 class="mb-2 font-weight-600">
-                    ROMANIA – GERMANIA Meci tur pentru calificarea la campionatul European de polo senioare, Budapesta 2020
-                </h4>
-                <div class="fs-13 mb-2 news">
-                    <span class="mr-2">FRPolo </span> <i class="fas fa-clock"></i> 10 Minutes ago
-                </div>
-                <br>
-                <a href="stire1.html" id="btnClr">Citeste</a>
+            <div class="row mt-5">
+                <?php while ($_posts->have_posts()): $_posts -> the_post();?>
 
-            </div>
+                <div class = "col-lg-4 text-center">
 
-        </div>
-        <br>
-        <div class="row mt-5 ">
-            <div class="col-sm-3 grid-margin">
-                <div class="position-relative">
-                    <div class="rotate-img">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/download.jfif" alt="thumb" class="img-fluid">
-                    </div>
+                <?php if(has_post_thumbnail()):?>
+                    <a href="<?php the_permalink() ?>">
+                    <img src="<?php the_post_thumbnail_url('small_image');?>" class="img-fluid mb-3" alt="<?php the_title();?>"> 
+                </a>
+                <?php endif; ?>
+                    <a href="<?php the_permalink() ?>"> 
+
+                        <h4><?php the_title();?></h4>
+
+                    </a>
+                    <?php the_excerpt();?>
 
                 </div>
-            </div>
-            <div class="col-sm-9  grid-margin text-left">
-                <h4 class="mb-2 font-weight-600">
-                    ROMANIA – GERMANIA Meci tur pentru calificarea la campionatul European de polo senioare, Budapesta 2020
-                </h4>
-                <div class="fs-13 mb-2 news">
-                    <span class="mr-2">FRPolo </span> <i class="fas fa-clock"></i> 10 Minutes ago
-                </div>
-                <br>
-                <a href="#" id="btnClr">Citeste</a>
-                <!-- <p class="mb-0">
-                    In data de 12 Octombrie 2019 la ora 19.00 echipa nationala de polo – senioare va intalni la Focsani selectionata Germaniei intru-un meci tur contand pentru calificarea la campionatul European de polo senioare, Budapesta 2020, urmand ca returul sa aiba
-                    loc in Germania la Duisburg pe data de 26 Octombrie 2019.
-                </p> -->
-                <!-- <button type="button" class="btn btn-light">Light</button> -->
-            </div>
-        </div>
-        <br>
-        <div class="row mt-5 ">
-            <div class="col-sm-3 grid-margin">
-                <div class="position-relative">
-                    <div class="rotate-img">
-                        <img src="<?php echo get_template_directory_uri();?>/img/download.jfif" alt="thumb" class="img-fluid">
-                    </div>
 
-                </div>
+                <?php endwhile; ?>
+
             </div>
-            <div class="col-sm-9  grid-margin text-left">
-                <h4 class="mb-2 font-weight-600">
-                    ROMANIA – GERMANIA Meci tur pentru calificarea la campionatul European de polo senioare, Budapesta 2020
-                </h4>
-                <div class="fs-13 mb-2 news">
-                    <span class="mr-2">FRPolo </span> <i class="fas fa-clock"></i> 10 Minutes ago
-                </div>
-                <br>
-                <a href="#" id="btnClr">Citeste</a>
-                <!-- <p class="mb-0">
-                    In data de 12 Octombrie 2019 la ora 19.00 echipa nationala de polo – senioare va intalni la Focsani selectionata Germaniei intru-un meci tur contand pentru calificarea la campionatul European de polo senioare, Budapesta 2020, urmand ca returul sa aiba
-                    loc in Germania la Duisburg pe data de 26 Octombrie 2019.
-                </p> -->
-                <!-- <button type="button" class="btn btn-light">Light</button> -->
-            </div>
-        </div>
-        <br>
-        <br>
-        <br>
+
+        <?php endif; ?>
+
     </div>
+    
+
+</div>
 
 
 
     <!--- Footer -->
-    <?php
-    get_footer();
-    ?>
- 
+<?php get_footer(); ?> 
